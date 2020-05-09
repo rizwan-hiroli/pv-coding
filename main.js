1231c570f19e6c01c9fd8f04cbb60af47dbe9cf7
Rizwan Hiroli <rizwanhiroli@gmail.com>
2020-05-09 23:10:46 +0530

    loader removed

--- main.js
+++ main.js
@@ -13,13 +13,10 @@ function sendRequest(url) {
           test = '<div class="column"><a href="'+'http://www.pinkvilla.com'+article.path+'" class="title-link"><img src="'+article.imageUrl+'" class="preview-image"><div class="row"><div class="title col-sm-9 col-md-9"> '+article.title+' </div><div class="pull-right col-sm-3 col-md-3" style="padding-top: 8px;"><i class="fas fa fa-ellipsis-h"></i></div></div></a></div>';
           document.getElementById("photos").innerHTML += test;
       })
-
     } else {
       console.log('error')
     }
-
-    document.getElementById("loader").style.display = 'none';
-
+    // document.getElementById("loader").style.display = 'none';
   }
 
   request.send();
